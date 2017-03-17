bc1c36e1ae05c678071820acab4513f8ded4e4da
Aaron Baluja <aaron.baluja@gmail.com>
2017-03-16 21:21:32 -0400

    Supports reading from four sensors Added serial data format Performance likely abysmal

--- test_linear_CCD/test_linear_CCD.ino
+++ test_linear_CCD/test_linear_CCD.ino
@@ -1,17 +1,27 @@
 //----------------------------------------------------------
 //based on code from http://playground.arduino.cc/Main/TSL1402R
 
+//Known Issues:
 //modified to read data serially as opposed to parallel
 //Cannot scope clk, causes failure in analog output. Output fine without scope probe
+
+//Usage:
+//Connect pins as described below. Note the variable assignment value is the pin on the Arduino, the comment next to the variable has the appropriate pin on the sensor
+//Ensure the VDD and GND pins are connected to the Arduino at the 5V and GND pins respectively
+
 
 // Define various ADC prescaler:
 const unsigned char PS_32 = (1 << ADPS2) | (1 << ADPS0);
 const unsigned char PS_128 = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-int CLKpin = 4;    // <-- Arduino pin delivering the clock pulses to pin 3 (CLK) of the TSL1402R 
-int SIpin = 5;     // <-- Arduino pin delivering the SI (serial-input) pulse to pin 2 of the TSL1402R 
-int AOpin1 = 1;    // <-- Arduino pin connected to pin 4 (analog output 1)of the TSL1402R
-int AOpin2 = 2;    // <-- Arduino pin connected to pin 8 (analog output 2)of the TSL1402R
-int IntArray[256]; // <-- the array where the readout of the photodiodes is stored, as integers
+uint8_t CLKpin = 4;    // <-- Arduino pin delivering the clock pulses to pin 3 (CLK) of all TSL1402R 
+uint8_t SIpin = 5;     // <-- Arduino pin delivering the SI (start/stop integration) pulse to pin 2 of all TSL1402R 
+
+//Arduino pins for each connected to AO1/AO2 to each of the TSL1402R
+uint8_t S1APin = 0;     
+uint8_t S2APin = 1;
+uint8_t S3APin = 2;
+uint8_t S4APin = 3;    
+
 
 void setup() 
 {
@@ -53,6 +63,14 @@ void setup()
 
 void loop() 
 {  
+
+          
+      
+      //arrays where the readout of the photodiodes is stored, as 16-bit integers
+      int S1Data[256];
+      int S2Data[256];
+      int S3Data[256];
+      int S4Data[256]; 
       // Stop the ongoing integration of light quanta from each photodiode by clocking in a SI pulse 
       // into the sensors register:
       digitalWrite(SIpin, HIGH);
@@ -64,7 +82,10 @@ void loop()
       for(int i=0; i < 256; i++)
       {
           delayMicroseconds(20);// <-- We add a delay to stabilize the AO output from the sensor
-          IntArray[i] = analogRead(AOpin1);
+          S1Data[i] = analogRead(S1APin);
+          S2Data[i] = analogRead(S2APin);
+          S3Data[i] = analogRead(S3APin);
+          S4Data[i] = analogRead(S4APin);
           ClockPulse(); 
       }
 
@@ -78,10 +99,35 @@ void loop()
       // communication. This takes ~80 ms during whick time no clock pulses reaches the sensor. 
       // No integration is taking place during this time from the photodiodes as the integration 
       // begins first after the 18th clock pulse after a SI pulse is inserted:
+
+      Serial.print("S1:");
       for(int i = 0; i < 256; i++)
       {
-          Serial.print(IntArray[i]); Serial.print(";");
+          Serial.print(S1Data[i]); Serial.print("|");
       }
+      Serial.print(";");
+      Serial.print("S2:");
+      for(int i = 0; i < 256; i++)
+      {
+          Serial.print(S2Data[i]); Serial.print("|");
+      }
+      Serial.print(";");
+            Serial.print("S3:");
+      for(int i = 0; i < 256; i++)
+      {
+          Serial.print(S3Data[i]); Serial.print("|");
+      }
+      Serial.print(";");
+            Serial.print("S4:");
+      for(int i = 0; i < 256; i++)
+      {
+          Serial.print(S4Data[i]); Serial.print("|");
+      }
+      Serial.print(";");
+      
+      
+      
+      
       Serial.println(""); // <-- Send a linebreak to indicate the measurement is transmitted.
 
       // Next, a new measuring cycle is starting once 18 clock pulses have passed. At  
@@ -90,11 +136,11 @@ void loop()
       // (by clocking in a new SI pulse):
       for(int i = 0; i < 260; i++)
       {
-          if(i==18)
-          {
-              // Now the photodiodes goes active..
-              // An external trigger can be placed here
-          }
+//          if(i==18)
+//          {
+//              // Now the photodiodes goes active..
+//              // An external trigger can be placed here
+//          }
           ClockPulse(); 
       }    
 
